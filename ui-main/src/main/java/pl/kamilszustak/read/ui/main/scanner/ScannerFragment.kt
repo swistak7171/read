c0d5e40f29345e06b74034736854461df98ede7e
Kamil Szustak <kamil.szustak98@gmail.com>
2020-10-11 15:09:53 +0200

    Add camera preview; #7

--- ui-main/src/main/java/pl/kamilszustak/read/ui/main/scanner/ScannerFragment.kt
+++ ui-main/src/main/java/pl/kamilszustak/read/ui/main/scanner/ScannerFragment.kt
@@ -1,11 +1,26 @@
 package pl.kamilszustak.read.ui.main.scanner
 
+import android.os.Bundle
+import android.view.View
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.afollestad.assent.Permission
+import com.afollestad.assent.askForPermissions
+import com.google.common.util.concurrent.ListenableFuture
 import pl.kamilszustak.read.ui.base.binding.viewBinding
+import pl.kamilszustak.read.ui.base.util.errorToast
 import pl.kamilszustak.read.ui.base.util.viewModels
 import pl.kamilszustak.read.ui.base.view.fragment.BaseFragment
 import pl.kamilszustak.read.ui.main.R
 import pl.kamilszustak.read.ui.main.databinding.FragmentScannerBinding
+import timber.log.Timber
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class ScannerFragment @Inject constructor(
@@ -14,4 +29,53 @@ class ScannerFragment @Inject constructor(
 
     override val viewModel: ScannerViewModel by viewModels(viewModelFactory)
     override val binding: FragmentScannerBinding by viewBinding(FragmentScannerBinding::bind)
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var camera: Camera
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            val permission = Permission.CAMERA
+            askForPermissions(permission) { result ->
+                val isGranted = result.isAllGranted(permission)
+                if (isGranted) {
+                    initializaCameraProvider()
+                } else {
+                    errorToast(R.string.camera_permission_not_granted)
+                }
+            }
+        }
+    }
+
+    private fun initializaCameraProvider() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder()
+            .build()
+
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+
+        val executor = Executors.newSingleThreadExecutor()
+        imageAnalysis.setAnalyzer(executor) { image ->
+            Timber.i(image.height.toString())
+        }
+
+        cameraProvider.unbindAll()
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview)
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+    }
 }
