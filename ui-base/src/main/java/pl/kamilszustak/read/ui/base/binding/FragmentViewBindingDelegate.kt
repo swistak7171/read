e7af4dbd50ad364bc6e61993244378037fd3b9fb
Kamil Szustak <kamil.szustak98@gmail.com>
2020-10-14 22:53:07 +0200

    Remove trailing commas; #10

--- ui-base/src/main/java/pl/kamilszustak/read/ui/base/binding/FragmentViewBindingDelegate.kt
+++ ui-base/src/main/java/pl/kamilszustak/read/ui/base/binding/FragmentViewBindingDelegate.kt
@@ -7,13 +7,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.observe
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class FragmentViewBindingDelegate<T : ViewBinding>(
     private val fragment: Fragment,
-    private val viewBindingFactory: (View) -> T,
+    private val viewBindingFactory: (View) -> T
 ) : ReadOnlyProperty<Fragment, T> {
 
     private var binding: T? = null
