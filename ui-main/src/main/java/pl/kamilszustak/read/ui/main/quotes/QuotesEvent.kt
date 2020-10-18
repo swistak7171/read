728c2e2f3ad67027c7152edd2cb51ad27ce90690
Kamil Szustak <kamil.szustak98@gmail.com>
2020-10-18 18:51:03 +0200

    Add popup menu to list items; #12

--- ui-main/src/main/java/pl/kamilszustak/read/ui/main/quotes/QuotesEvent.kt
+++ ui-main/src/main/java/pl/kamilszustak/read/ui/main/quotes/QuotesEvent.kt
@@ -1,7 +1,11 @@
 package pl.kamilszustak.read.ui.main.quotes
 
+import pl.kamilszustak.model.common.id.QuoteId
 import pl.kamilszustak.read.ui.base.view.ViewEvent
 
 sealed class QuotesEvent : ViewEvent {
     object OnAddQuoteButtonClicked : QuotesEvent()
+    data class OnEditQuoteButtonClicked(
+        val quoteId: QuoteId,
+    ) : QuotesEvent()
 }
--- ui-main/src/main/java/pl/kamilszustak/read/ui/main/quotes/QuotesFragment.kt
+++ ui-main/src/main/java/pl/kamilszustak/read/ui/main/quotes/QuotesFragment.kt
@@ -1,11 +1,15 @@
 package pl.kamilszustak.read.ui.main.quotes
 
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ModelAdapter
+import com.mikepenz.fastadapter.listeners.ClickEventHook
 import pl.kamilszustak.read.model.domain.Quote
 import pl.kamilszustak.read.ui.base.binding.viewBinding
 import pl.kamilszustak.read.ui.base.util.navigate
+import pl.kamilszustak.read.ui.base.util.popupMenu
 import pl.kamilszustak.read.ui.base.util.updateModels
 import pl.kamilszustak.read.ui.base.util.viewModels
 import pl.kamilszustak.read.ui.base.view.fragment.BaseFragment
@@ -24,7 +28,41 @@ class QuotesFragment @Inject constructor(
     }
 
     override fun initializeRecyclerView() {
-        val fastAdapter = FastAdapter.with(modelAdapter)
+        val fastAdapter = FastAdapter.with(modelAdapter).apply {
+            addEventHook(object : ClickEventHook<QuoteItem>() {
+                override fun onBind(viewHolder: RecyclerView.ViewHolder): View? {
+                    return if (viewHolder is QuoteItem.ViewHolder) {
+                        viewHolder.binding.menuButton
+                    } else {
+                        null
+                    }
+                }
+
+                override fun onClick(
+                    v: View,
+                    position: Int,
+                    fastAdapter: FastAdapter<QuoteItem>,
+                    item: QuoteItem
+                ) {
+                    popupMenu(v, R.menu.popup_menu_quote_item) {
+                        setForceShowIcon(true)
+                        setOnMenuItemClickListener { menuItem ->
+                            when (menuItem.itemId) {
+                                R.id.editQuoteItem -> {
+                                    val event = QuotesEvent.OnEditQuoteButtonClicked(item.model.id)
+                                    viewModel.dispatchEvent(event)
+                                    true
+                                }
+
+                                else -> {
+                                    false
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+        }
         binding.quotesRecyclerView.apply {
             adapter = fastAdapter
         }
