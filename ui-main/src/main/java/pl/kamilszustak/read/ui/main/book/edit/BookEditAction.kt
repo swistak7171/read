89b56ed22559f059695418ad4111a8c33db11373
Kamil Szustak <kamil.szustak98@gmail.com>
2020-11-01 12:05:06 +0100

    Set date picker initial selection if publication date has been selected; #22

--- ui-main/src/main/java/pl/kamilszustak/read/ui/main/book/edit/BookEditAction.kt
+++ ui-main/src/main/java/pl/kamilszustak/read/ui/main/book/edit/BookEditAction.kt
@@ -2,10 +2,14 @@ package pl.kamilszustak.read.ui.main.book.edit
 
 import androidx.annotation.StringRes
 import pl.kamilszustak.read.ui.base.view.ViewAction
+import java.util.*
 
 sealed class BookEditAction : ViewAction {
-    object OpenDatePicker : BookEditAction()
     object NavigateUp : BookEditAction()
+
+    data class OpenDatePicker(
+        val selectedDate: Date,
+    ) : BookEditAction()
 
     data class Error(
         @StringRes val messageResourceId: Int,
--- ui-main/src/main/java/pl/kamilszustak/read/ui/main/book/edit/BookEditFragment.kt
+++ ui-main/src/main/java/pl/kamilszustak/read/ui/main/book/edit/BookEditFragment.kt
@@ -13,8 +13,8 @@ import pl.kamilszustak.read.common.util.useOrNull
 import pl.kamilszustak.read.ui.base.util.errorToast
 import pl.kamilszustak.read.ui.base.util.navigateUp
 import pl.kamilszustak.read.ui.base.util.successToast
-import pl.kamilszustak.read.ui.main.R
 import pl.kamilszustak.read.ui.main.MainDataBindingFragment
+import pl.kamilszustak.read.ui.main.R
 import pl.kamilszustak.read.ui.main.databinding.FragmentBookEditBinding
 import java.util.*
 import javax.inject.Inject
@@ -65,8 +65,8 @@ class BookEditFragment @Inject constructor(
 
         viewModel.action.observe(viewLifecycleOwner) { action ->
             when (action) {
-                BookEditAction.OpenDatePicker -> {
-                    openDatePicker()
+                is BookEditAction.OpenDatePicker -> {
+                    openDatePicker(action)
                 }
 
                 is BookEditAction.Error -> {
@@ -84,9 +84,9 @@ class BookEditFragment @Inject constructor(
         }
     }
 
-    private fun openDatePicker() {
+    private fun openDatePicker(action: BookEditAction.OpenDatePicker) {
         val datePicker = MaterialDatePicker.Builder.datePicker()
-            .setSelection(System.currentTimeMillis())
+            .setSelection(action.selectedDate.time)
             .setTitleText(R.string.select_a_publication_date)
             .build()
 
