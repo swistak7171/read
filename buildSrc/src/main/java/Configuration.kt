0237940fe7e5cc849da581b3d0aeb6afb13b2f00
Kamil Szustak <kamil.szustak98@gmail.com>
2020-12-14 12:05:14 +0100

    Increment version

--- buildSrc/src/main/java/Configuration.kt
+++ buildSrc/src/main/java/Configuration.kt
@@ -4,6 +4,6 @@ object Configuration {
     const val MIN_SDK_VERSION: Int = 23
     const val TARGET_SDK_VERSION: Int = 29
     const val BUILD_TOOLS_VERSION: String = "29.0.3"
-    const val VERSION_CODE: Int = 9
-    const val VERSION_NAME: String = "0.8.0"
+    const val VERSION_CODE: Int = 10
+    const val VERSION_NAME: String = "0.8.1"
 }
